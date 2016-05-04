366bd7b2c10bc4f1bbbbbdf48691969608cf11d3
HrycajF <hrycajf@gmail.com>
2016-05-04 17:09:41 +0200

    Tasks Tabellenabfrage integriert

--- CNC/Interface/js/script.js
+++ CNC/Interface/js/script.js
@@ -1,8 +1,9 @@
 /* Aufrufe */
 getStatusData();
-
+getTasksData();
 
 /* Funktionen */
+/* Status */
 function getStatusData(){
   var xhrStatusData = new XMLHttpRequest();
 
@@ -10,22 +11,80 @@ function getStatusData(){
   xhrStatusData.responseType = 'json';
 
   xhrStatusData.onload = function() {
-    var data = xhrStatusData.response;
+    var statusData = xhrStatusData.response;
 
-    if (data !== null) {
-        createStatusTable(data);
+    if (statusData !== null) {
+        createStatusTable(statusData);
     }
   };
   xhrStatusData.send(null);
-  setTimeout(getStatusData, 30000);
+}
+
+function createStatusTable(statusData) {
+  var statusTableArray = statusData;
+  var i;
+  var statusOut = "";
+
+  for(i = 0; i < statusTableArray.length; i++) {
+      statusOut += "<tr><td>" +
+      statusTableArray[i].id +
+      "</td><td>" +
+      statusTableArray[i].ip +
+      "</td><td>" +
+      statusTableArray[i].task +
+      "</td><td>" +
+      statusTableArray[i].workload +
+      "</td><td>" + '<button onclick="toggleName(this.id)" type="button" id="'
+          + statusTableArray[i].id + 'stop" >Stop</button>' + '</td>';
+      "</td></tr>";
+
+  document.getElementById("dynamicTableStatus").innerHTML = statusOut;
+  }
 }
 
 
-//Tasks Formular POST
+/* Tasks */
+function getTasksData(){
+  var xhrTasksData = new XMLHttpRequest();
+
+  xhrTasksData.open('GET', 'http://botnet.artificial.engineering:8080/api/Tasks/');
+  xhrTasksData.responseType = 'json';
+
+  xhrTasksData.onload = function() {
+    var tasksData = xhrTasksData.response;
+
+    if (tasksData !== null) {
+        createTasksTable(tasksData);
+    }
+  };
+  xhrTasksData.send(null);
+}
+
+function createTasksTable(tasksData) {
+  var tasksTableArray = tasksData;
+  var j;
+  var tasksOut = "";
+
+  for(j = 0; j < tasksTableArray.length; j++) {
+      tasksOut += "<tr><td>" +
+      tasksTableArray[j].id +
+      "</td><td>" +
+      tasksTableArray[j].type +
+      "</td><td>" +
+      tasksTableArray[j].data.input +
+      "</td><td>" +
+      tasksTableArray[j].data.output +
+      "</td></tr>";
+
+  document.getElementById("dynamicTableTasks").innerHTML = tasksOut;
+  }
+}
+
+
 function submitTaskData() {
   var xhrTaskData = new XMLHttpRequest();
 
-  xhrTaskData.open('POST', 'http://botnet.artificial.engineering:8080/api/tasks/');
+  xhrTaskData.open('POST', 'http://botnet.artificial.engineering:8080/api/Tasks/');
   xhrTaskData.responseType = 'json';
   xhrTaskData.setRequestHeader("WAW404", "f971f01d8805354730fefa0c897a737f");
 
@@ -37,6 +96,17 @@ function submitTaskData() {
 
   xhrTaskData.send(JSON.stringify(myJson));
 }
+
+var toggleName = function(clicked_id){
+      //Start/Stop toggle
+    if(document.getElementById(clicked_id).innerHTML == "Stop"){
+      document.getElementById(clicked_id).innerHTML = "Start";
+    }
+    else if(document.getElementById(clicked_id).innerHTML == "Start"){
+      document.getElementById(clicked_id).innerHTML = "Stop";
+    }
+  };
+
 
 
 /* function postRequest() {
@@ -66,49 +136,3 @@ function submitTaskData() {
 
 postRequest();
 */
-
-function fetchData(){
-
-  var data = document.querySelector('#blablubb');
-
-
-}
-
-function createStatusTable(data) {
-  var myArray = data;
-  var i;
-  var out = "";
-
-  for(i = 0; i < myArray.length; i++) {
-      out += "<tr><td>" +
-      myArray[i].workload +
-      "</td><td>" +
-      myArray[i].ip +
-      "</td><td>" +
-      myArray[i].task + " (" + myArray[i].id + ")" +
-      "</td><td>" /* + '<button onclick="toggleName(this.id)" type="button" id="'+ myArray[i].id + 'pause" >Pause</button>' */
-                  + '<button onclick="toggleName(this.id)" type="button" id="'+ myArray[i].id + 'stop" >Stop</button>' + '</td>';
-      "</td></tr>";
-
-  document.getElementById("dynamicTableStatus").innerHTML = out;
-  }
-}
-
-
-var toggleName = function(clicked_id){
-      //Start/Stop toggle
-    if(document.getElementById(clicked_id).innerHTML == "Stop"){
-      document.getElementById(clicked_id).innerHTML = "Start";
-    }
-    else if(document.getElementById(clicked_id).innerHTML == "Start"){
-      document.getElementById(clicked_id).innerHTML = "Stop";
-    }
-
-    //Pause/Resume toggle
-    if(document.getElementById(clicked_id).innerHTML == "Pause"){
-      document.getElementById(clicked_id).innerHTML = "Resume";
-    }
-    else if(document.getElementById(clicked_id).innerHTML == "Resume"){
-      document.getElementById(clicked_id).innerHTML = "Pause";
-    }
-  };
