c7ed8dc84300754878ff95b7dd9a4d441f35e0ff
HrycajF <hrycajf@gmail.com>
2016-05-07 20:28:33 +0200

    JavaScript Funktionen zusammen gefasst

--- CNC/Interface/js/script.js
+++ CNC/Interface/js/script.js
@@ -1,8 +1,7 @@
- /* Aufrufe */
+ /* Aufrufe beim Öffnen der Seite */
 getStatusData();
 getTasksData();
 
-/* Funktionen */
 /* Status */
 function getStatusData(){
   var xhrStatusData = new XMLHttpRequest();
@@ -12,9 +11,45 @@ function getStatusData(){
 
   xhrStatusData.onload = function() {
     var statusData = xhrStatusData.response;
-
     if (statusData !== null) {
-        createStatusTable(statusData);
+      var statusOut = "";
+      for(var i = 0; i < statusData.length; i++) {
+          var json_data = {};
+          json_data['id'] = Number(statusData[i].id);
+
+          var buttonPart = "";
+          var buttonHead = "";
+
+          if(statusData[i].workload === 0){
+            json_data['status'] = true;
+            buttonPart = '"style="background-color: green; color: white; border-style: none; padding: 3px;">Start</button>' + '</td>';
+
+          }else if(statusData[i].workload === 1){
+            json_data['status'] = false;
+            buttonPart = '" style="background-color: red; color: white; border-style: none; padding: 3px;">Stop</button>' + '</td>';
+          }
+
+          //ID und Status werden einfach als ID für die Buttons verwendet. Dafür muss der String aber escaped werden,
+          // da es sonst zu Problemen mit den Anführungszeichen kommt
+
+          var button_id = JSON.stringify(json_data);
+          buttonHead += '<button onclick="sendPostRequest(this.id)" type="button" id="' +  escape(button_id);
+
+          var button = "";
+          button = buttonHead + buttonPart;
+
+          statusOut += "<tr><td>" +
+          statusData[i].id +
+          "</td><td>" +
+          statusData[i].ip +
+          "</td><td>" +
+          statusData[i].task +
+          "</td><td>" +
+          statusData[i].workload +
+          "</td><td>" +
+          button + "</td></tr>";
+      }
+      document.getElementById("dynamicTableStatus").innerHTML = statusOut;
     }
   };
   xhrStatusData.send(null);
@@ -22,7 +57,7 @@ function getStatusData(){
 
 
 /* Tasks */
-function getTasksData(){
+function getTasksData() {
   var xhrTasksData = new XMLHttpRequest();
 
   xhrTasksData.open('GET', 'http://botnet.artificial.engineering:8080/api/Tasks/');
@@ -30,40 +65,32 @@ function getTasksData(){
 
   xhrTasksData.onload = function() {
     var tasksData = xhrTasksData.response;
-
     if (tasksData !== null) {
-        createTasksTable(tasksData);
+      var tasksOut = "";
+      for(var j = 0; j < tasksData.length; j++) {
+          tasksOut += "<tr><td>" +
+          tasksData[j].id +
+          "</td><td>" +
+          tasksData[j].type +
+          "</td><td>" +
+          tasksData[j].data.input +
+          "</td><td>" +
+          tasksData[j].data.output +
+          "</td></tr>";
+      }
+      document.getElementById('dynamicTableTasks').innerHTML = tasksOut;
     }
   };
   xhrTasksData.send(null);
 }
-
-function createTasksTable(tasksData) {
-  var tasksTableArray = tasksData;
-  var j;
-  var tasksOut = "";
-
-  for(j = 0; j < tasksTableArray.length; j++) {
-      tasksOut += "<tr><td>" +
-      tasksTableArray[j].id +
-      "</td><td>" +
-      tasksTableArray[j].type +
-      "</td><td>" +
-      tasksTableArray[j].data.input +
-      "</td><td>" +
-      tasksTableArray[j].data.output +
-      "</td></tr>";
-  }
-  document.getElementById("dynamicTableTasks").innerHTML = tasksOut;
-}
-
 
 function submitTaskData() {
   var xhrTaskData = new XMLHttpRequest();
 
   xhrTaskData.open('POST', 'http://botnet.artificial.engineering:8080/api/Tasks/');
   xhrTaskData.responseType = 'json';
-  xhrTaskData.setRequestHeader("WAW404", "f971f01d8805354730fefa0c897a737f");
+  xhrTaskData.setRequestHeader('Content-Type', 'application/json');
+  xhrTaskData.setRequestHeader('token', 'f971f01d8805354730fefa0c897a737f');
 
   var myJson = {
     id: document.getElementById('id').value,
@@ -71,8 +98,38 @@ function submitTaskData() {
     data: document.getElementById('data').value
   };
 
+  xhrTaskData.onload = function (e) {
+      if (this.status == 200) {
+          var data = xhrTaskData.response;
+          if (data !== null && data.message != "OK")
+              alert("ERROR");
+      }
+  };
+
   xhrTaskData.send(JSON.stringify(myJson));
 }
+
+function sendPost(api, json, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', botnet+api, true);
+    xhr.responseType = "json";
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('token', token);
+
+    xhr.onload = function (e) {
+        if (this.status == 200) {
+            var data = xhr.response;
+            if (data !== null && data.message != "OK")
+                alert("ERROR");
+            else
+                callback();
+        }
+    };
+
+    xhr.send(json);
+}
+
+
 
 
 function fetchData(){
@@ -82,55 +139,7 @@ function fetchData(){
 
 }
 
-function createStatusTable(statusData) {
-  var myArray = statusData;
-  var i;
-  var statusOut = "";
 
-  for(i = 0; i < myArray.length; i++) {
-
-      var json_data = {};
-
-      json_data['id'] = Number(myArray[i].id);
-
-      var buttonPart = "";
-      var buttonHead = "";
-
-      if(myArray[i].workload === 0){
-
-        json_data['status'] = true;
-        buttonPart = '"style="background-color: green; color: white; border-style: none; padding: 3px;">Start</button>' + '</td>';
-
-      }else if(myArray[i].workload === 1){
-
-        json_data['status'] = false;
-        buttonPart = '" style="background-color: red; color: white; border-style: none; padding: 3px;">Stop</button>' + '</td>';
-
-      }
-
-      //ID und Status werden einfach als ID für die Buttons verwendet. Dafür muss der String aber escaped werden,
-      // da es sonst zu Problemen mit den Anführungszeichen kommt
-
-      var button_id = JSON.stringify(json_data);
-      buttonHead += '<button onclick="sendPostRequest(this.id)" type="button" id="' +  escape(button_id);
-
-      var button = "";
-      button = buttonHead + buttonPart;
-
-
-      statusOut += "<tr><td>" +
-      myArray[i].id +
-      "</td><td>" +
-      myArray[i].ip +
-      "</td><td>" +
-      myArray[i].task +
-      "</td><td>" +
-      myArray[i].workload +
-      "</td><td>" +
-      button + "</td></tr>";
-  }
-  document.getElementById("dynamicTableStatus").innerHTML = statusOut;
-}
 
 function setStatusFlag(json_data){
 
